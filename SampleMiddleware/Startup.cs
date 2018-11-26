e81a29ce4f73e5e99199e9d6a818f8c8412ba44e
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-11-26 14:22:52 +0700

    menambahkan controller

--- SampleMiddleware/Startup.cs
+++ SampleMiddleware/Startup.cs
@@ -18,6 +18,7 @@ namespace SampleMiddleware
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,6 +31,8 @@ namespace SampleMiddleware
             }
 
             app.UseStaticFiles();
+            //{controller=Home}/{action=Index}
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
